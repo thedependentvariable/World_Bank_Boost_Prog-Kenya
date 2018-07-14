88c5938d16ee1e613bde37b55e07449ae73772a4
Odhiambo1 <maxtony624@gmail.com>
2018-07-14 16:48:36 +0300

    Boost Analysis

--- Analysis.R
+++ Analysis.R
@@ -4,7 +4,7 @@
 library(tidyverse)
 library(ggthemes)
 library(DataExplorer)
-
+library(plotly)
 #load data
 
 kenya_boost_2018<-read.csv(file = "kenya_boost_2013-2018.csv",colClasses="character") 
@@ -28,6 +28,10 @@ kenya_boost[,col2cvt] <- lapply(kenya_boost[,col2cvt],function(kenya_boost){as.n
 
 #fill NA's with 0, there were no funds allocated
 kenya_boost[is.na(kenya_boost)]<-0
+
+
+#merge same periods with different formats to simmilar format 
+kenya_boost[,1] <- lapply(kenya_boost[,1],function(kenya_boost){as.numeric(gsub("2014-15", "2014-15", kenya_boost))})
 
 #quick view of the data
 introduce(kenya_boost)
@@ -53,3 +57,22 @@ yearly_budget<-kenya_boost%>%
 
 #plot of the number of projects funded in a year.
 ggplot(yearly_budget,aes(x= Year,y=Number_allocations,fill=Year))+geom_bar(stat = "identity",show.legend = FALSE)+ ggtitle("Number of projects funded per year")
+
+
+#Which  class has benefited the most in each years boost
+class_allocations<-kenya_boost %>% 
+  group_by(Year,Class) %>% 
+  summarise(allocations = n(),
+            total_budget = sum(Final.Budget..Approved.Estimate.))
+
+ggplot(class_allocations,aes(x=Class,y=allocations,fill= Year))+
+  geom_bar(stat = "identity",show.legend = FALSE)+
+  facet_wrap(~Year)+ggtitle("Number of allocations per year")+
+  theme_hc()
+
+#budgeting per project per year
+p<-ggplot(class_allocations,aes(x=Class,y=total_budget,fill= Year))+
+  geom_bar(stat = "identity",show.legend = FALSE)+
+  facet_wrap(~Year)+ggtitle("Budget per project per year")+
+  theme_wsj()
+ggplotly(p)
